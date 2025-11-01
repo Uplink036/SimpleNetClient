58aacc6593a3b0d78dae7d0bf90793bba8d762b7
Uplink036 <olsj21@student.bth.se>
2025-11-01 23:14:23 +0000

    Fix protocol linting issues

--- src/server/protocol.h
+++ src/server/protocol.h
@@ -1,32 +1,29 @@
-#ifndef PROTOCOL_H
-#define PROTOCOL_H
+#ifndef SRC_SERVER_PROTOCOL_H_
+#define SRC_SERVER_PROTOCOL_H_
 
 #include <unistd.h>
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include "ip.h"
-#include "debug.h"
-#include "macros.h"
+#include "src/lib/ip.h"
+#include "src/debug.h"
+#include "src/lib/macros.h"
 
 #define MAX_MESSGE_SIZE 1500
 
 extern const char* SERVER_PROTOCOLS[];
 
-enum CommunicationProtocl
-{
+enum CommunicationProtocl {
   TEXT,
   BINARY
 };
 
-enum InternetProtocl
-{
+enum InternetProtocl {
   TCP,
   UDP
 };
 
-struct ApplicationProtocl
-{
+struct ApplicationProtocl {
   CommunicationProtocl com;
   InternetProtocl proc;
 };
@@ -35,4 +32,4 @@ typedef struct ApplicationProtocl ApplicationProtocl;
 bool sendServerProtocol(int client_fd);
 bool getClientProtocol(int client_fd, ApplicationProtocl* clientProtocol);
 
-#endif
+#endif  // SRC_SERVER_PROTOCOL_H_
