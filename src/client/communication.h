3001fb46448a75166aa3c7f1d839504f803f211f
Uplink036 <olsj21@student.bth.se>
2025-11-01 23:08:00 +0000

    Fix communication linting issues

--- src/client/communication.h
+++ src/client/communication.h
@@ -1,5 +1,5 @@
-#ifndef COMMUNICATION_H
-#define COMMUNICATION_H
+#ifndef SRC_CLIENT_COMMUNICATION_H_
+#define SRC_CLIENT_COMMUNICATION_H_
 
 
 #include <stdio.h>
@@ -10,17 +10,19 @@
 #include <netdb.h>
 #include <ctype.h>
 #include <fcntl.h>
-#include "ip.h"
-#include "debug.h"
-#include "calc.h"
-#include "netparser.h"
-#include "macros.h"
+#include "src/lib/ip.h"
+#include "src/debug.h"
+#include "src/lib/calc.h"
+#include "src/client/netparser.h"
+#include "src/lib/macros.h"
 
 
 int getServerTask(int socketfd, char* msg);
-bool getServerProtocols(int socketfd, char* expected_protocol, fd_set* fdset, timeval* tv);
-int sendClientProtocol(bool foundProtocl, int socketfd, char pathstring[7], char protocolstring[6]);
+bool getServerProtocols(int socketfd, char* expected_protocol, fd_set* fdset,
+                        timeval* tv);
+int sendClientProtocol(bool foundProtocl, int socketfd, char pathstring[7],
+                       char protocolstring[6]);
 int sendResultToServer(int result, int socketfd);
 void getResultResponseBack(int socketfd, int result);
 
-#endif
+#endif  // SRC_CLIENT_COMMUNICATION_H_
