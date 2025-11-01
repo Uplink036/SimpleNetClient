4870b30cdd3675039f99ea18adb234889b1ae051
Uplink036 <olsj21@student.bth.se>
2025-11-01 23:02:41 +0000

    Fix calculateTask linting issues

--- src/client/calculateTask.cpp
+++ src/client/calculateTask.cpp
@@ -1,7 +1,6 @@
-#include "calculateTask.h"
+#include "src/client/calculateTask.h"
 
-enum op stringToOp(char* input)
-{
+enum op stringToOp(char* input) {
   DEBUG_FUNCTION("client::main::stringToOp(%s)", input);
   IF_ZERO(strcmp("add", input))
     return op::ADD;
@@ -17,22 +16,19 @@ enum op stringToOp(char* input)
 }
 
 
-int calculateServerTask(char* msg)
-{
+int calculateServerTask(char* msg) {
   DEBUG_FUNCTION("client::main::calculateServerTask(%s)\n", msg);
   char operation[10];
   int valueOne, valueTwo;
-  IF_NEGATIVE(sscanf(msg, "%s %d %d", operation, &valueOne, &valueTwo))
-  {
+  IF_NEGATIVE(sscanf(msg, "%s %d %d", operation, &valueOne, &valueTwo)) {
     printf("ERROR\n");
     DEBUG_FUNCTION("Could not properly read scaned values - %s", msg);
     exit(EXIT_FAILURE);
-  }    
+  }
   printf("ASSIGNMENT: %s %d %d\n", operation, valueOne, valueTwo);
   int result;
   double temp;
-  switch (stringToOp(operation))
-  {
+  switch (stringToOp(operation)) {
   case op::ADD:
       result = valueOne + valueTwo;
       break;
