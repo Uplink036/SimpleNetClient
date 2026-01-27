cc4b97a53886de7bf728e0f8922d64902d645282
Uplink036 <olsj21@student.bth.se>
2026-01-27 08:32:46 +0000

    Add distinction between TCP and UDP

---
+++ src/client/tcp.h
@@ -0,0 +1,4 @@
+#ifndef SRC_CLIENT_TCP_H_
+#define SRC_CLIENT_TCP_H_
+
+#endif  // SRC_CLIENT_TCP_H_
---
+++ src/client/udp.h
@@ -0,0 +1,4 @@
+#ifndef SRC_CLIENT_UDP_H_
+#define SRC_CLIENT_UDP_H_
+
+#endif  // SRC_CLIENT_UDP_H_
