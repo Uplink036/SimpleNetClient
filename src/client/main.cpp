9cf40e8a98e24f59f8ce31fe27f16ff03b484ee4
Uplink036 <olsj21@student.bth.se>
2026-03-12 21:33:55 +0000

    feat: Add switch to Main.py

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -8,8 +8,16 @@ int main(int argc, char *argv[]) {
   char* destinationPort;
   parseInputArgs(argv, protocolstring, pathstring, &destination,
                  &destinationPort);
-  exitStatus = connectTCP(destination, destinationPort, pathstring,
-                          protocolstring);
+  if (strcmp(protocolstring, "TCP") == 0) {
+    exitStatus = connectTCP(destination, destinationPort, pathstring,
+                            protocolstring);
+  } else if (strcmp(protocolstring, "UDP") == 0) {
+    exitStatus = connectUDP(destination, destinationPort, pathstring,
+                            protocolstring);
+  } else {
+    printf("ERROR: UNSUPPORTED TRANSPORT PROTOCOL\n");
+    exitStatus = 1;
+  }
   free(destination);
   free(destinationPort);
   return exitStatus;
